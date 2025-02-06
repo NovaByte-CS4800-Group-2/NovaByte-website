1d3176083c388348ff4afd9ffb36b89d7d4318a6
turtlelover56 <anadavis@gmail.com>
2025-02-06 13:01:38 -0800

    Added functionality to read and display log data from logs.csv

--- logs.js
+++ logs.js
@@ -1,109 +1,64 @@
 // Get the week dropdown selector from logs.html and the week display to show the log information.
 const weekDropdown = document.getElementById("week-dropdown");
 const weekDisplay = document.getElementById("week-display");
+
+// Parse the file.
+const fileName = "logs.csv";
+const promise = new Promise((resolve, reject) => {
+	fetch(fileName)
+		.then(response => response.text())
+		.then(text => {
+			Papa.parse(text, {
+				header: true,
+				complete: function(results) {
+					resolve(results.data);
+				},
+				error: function(error) {
+					reject(error);
+				}
+			})
+		});
+});
+// Store the log data as an array in "data".
+const data = await promise.then(result => {return result});
+// Array for days of the week.
+const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
 // Add event listener for change in value with an arrow function.
 weekDropdown.addEventListener("change", () => {
+	// If the dropdown value is the default "select" option, clear the display.
 	if (weekDropdown.value === "select") {
 		weekDisplay.innerHTML = "";
 	}
 	else {
-		weekDisplay.innerHTML = `<ul>Week 1
-					<li>Monday, February 3
-						<dl>What we accomplished:
-							<ol>
-								<li class="smaller-text">Ana - Resarched project topics</li>
-								<li class="smaller-text">Natalie - Resarched project topics</li>
-								<li class="smaller-text">Jessica - Resarched project topics</li>
-								<li class="smaller-text">Hadya - Resarched project topics</li>
-								<li class="smaller-text">Arin - Resarched project topics</li>
-
-							</ol>
-						<dl>What we want to accomplish: 
-							<ol>
-								<li class="smaller-text">Ana - Resarched tech stack and more concepts</li>
-								<li class="smaller-text">Natalie - Resarched tech stack and more concepts</li>
-								<li class="smaller-text">Jessica - Resarched tech stack and more concepts</li>
-								<li class="smaller-text">Hadya - Resarched tech stack and more concepts</li>
-								<li class="smaller-text">Arin - Resarched tech stack and more concepts</li>
-							</ol>
-						</dl>
-					</li>
-					<li>Tuesday, February 4
-						<dl>What we accomplished:
-							<ol>
-								<li class="smaller-text">Ana - Completed this</li>
-								<li class="smaller-text">Natalie - Completed that</li>
-								<li class="smaller-text">Jessica - Completed final</li>
-								<li class="smaller-text">Hadya - Start this</li>
-								<li class="smaller-text">Arin - Complete that</li>
-							</ol>
-						<dl>What we want to accomplish: 
-							<ol>
-								<li class="smaller-text">Ana - Start this</li>
-								<li class="smaller-text">Natalie - Complete that</li>
-								<li class="smaller-text">Jessica - Work on final</li>
-								<li class="smaller-text">Hadya - Start this</li>
-								<li class="smaller-text">Arin - Complete that</li>
-							</ol>
-						</dl>
-					</li>
-					<li>Wednesday, February 5
-						<dl>What we accomplished:
-							<ol>
-								<li class="smaller-text">Ana - Completed this</li>
-								<li class="smaller-text">Natalie - Completed that</li>
-								<li class="smaller-text">Jessica - Completed final</li>
-								<li class="smaller-text">Hadya - Start this</li>
-								<li class="smaller-text">Arin - Complete that</li>
-							</ol>
-						<dl>What we want to accomplish: 
-							<ol>
-								<li class="smaller-text">Ana - Start this</li>
-								<li class="smaller-text">Natalie - Complete that</li>
-								<li class="smaller-text">Jessica - Work on final</li>
-								<li class="smaller-text">Hadya - Start this</li>
-								<li class="smaller-text">Arin - Complete that</li>
-							</ol>
-						</dl>
-					</li>
-					<li>Thursday, February 6
-						<dl>What we accomplished:
-							<ol>
-								<li class="smaller-text">Ana - Completed this</li>
-								<li class="smaller-text">Natalie - Completed that</li>
-								<li class="smaller-text">Jessica - Completed final</li>
-								<li class="smaller-text">Hadya - Start this</li>
-								<li class="smaller-text">Arin - Complete that</li>
-							</ol>
-						<dl>What we want to accomplish: 
-							<ol>
-								<li class="smaller-text">Ana - Start this</li>
-								<li class="smaller-text">Natalie - Complete that</li>
-								<li class="smaller-text">Jessica - Work on final</li>
-								<li class="smaller-text">Hadya - Start this</li>
-								<li class="smaller-text">Arin - Complete that</li>
-							</ol>
-						</dl>
-					</li>
-					<li>Friday, February 7
-						<dl>What we accomplished:
-							<ol>
-								<li class="smaller-text">Ana - Completed this</li>
-								<li class="smaller-text">Natalie - Completed that</li>
-								<li class="smaller-text">Jessica - Completed final</li>
-								<li class="smaller-text">Hadya - Start this</li>
-								<li class="smaller-text">Arin - Complete that</li>
-							</ol>
-						<dl>What we want to accomplish: 
-							<ol>
-								<li class="smaller-text">Ana - Start this</li>
-								<li class="smaller-text">Natalie - Complete that</li>
-								<li class="smaller-text">Jessica - Work on final</li>
-								<li class="smaller-text">Hadya - Start this</li>
-								<li class="smaller-text">Arin - Complete that</li>
-							</ol>
-						</dl>
-					</li>
-				</ul>`
+		// Get the chosen week's log data.
+		const weekNumber = weekDropdown.value.at(-1);
+		const thisWeek = data.filter(day => day["week-number"] === weekNumber);
+		// Create the html to display.
+		let html = `<ul>Week ${weekNumber}`
+		// Add the data for each day of the chosen week.
+		for (const day of thisWeek) {
+			html += `<li>${weekdays[parseInt(day.entry) - 1]}, ${day.date}
+					<dl>What we accomplished:
+						<ol>
+							<li class="smaller-text">Natalie - ${day["Natalie-1"]}</li>
+							<li class="smaller-text">Arin - ${day["Arin-1"]}</li>
+							<li class="smaller-text">Anastasia - ${day["Anastasia-1"]}</li>
+							<li class="smaller-text">Jessica - ${day["Jessica-1"]}</li>
+							<li class="smaller-text">Hadya - ${day["Hadya-1"]}</li>
+						</ol>
+					<dl>What we want to accomplish: 
+						<ol>
+							<li class="smaller-text">Natalie - ${day["Natalie-2"]}</li>
+							<li class="smaller-text">Arin - ${day["Arin-2"]}</li>
+							<li class="smaller-text">Anastasia - ${day["Anastasia-2"]}</li>
+							<li class="smaller-text">Jessica - ${day["Jessica-2"]}</li>
+							<li class="smaller-text">Hadya - ${day["Hadya-2"]}</li>
+						</ol>
+					</dl>
+				</li>`
+		}
+		// Set the week display to show the log for that week.
+		weekDisplay.innerHTML = html;
 	}
 });
